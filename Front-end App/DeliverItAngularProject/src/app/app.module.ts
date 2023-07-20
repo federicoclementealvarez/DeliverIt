3dd2cc265506b65fce07bdff5b83d7f9316f94f2
fabriortenzi <fabriortenzi10@gmail.com>
2023-07-20 11:19:43 -0300

    Primary button added

--- Front-end App/DeliverItAngularProject/src/app/app.module.ts
+++ Front-end App/DeliverItAngularProject/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimaryButtonComponent } from './primary-button/primary-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryButtonComponent
   ],
   imports: [
     BrowserModule,
