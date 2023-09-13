223c41fbf0d0ff0f4d9420a2a58bf09241c165dd
fabriortenzi <fabriortenzi10@gmail.com>
2023-09-13 08:51:41 -0300

    CRUD ProductCategory added

--- Back-end App/src/app.ts
+++ Back-end App/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { shopTypeRouter } from './shopType/shopType.routes.js';
 import { paymentTypeRouter } from './paymentType/paymentType.routes.js';
+import { productCategoryRouter } from './productCategory/productCategory.routes.js';
 
 const app = express();
 app.use(express.json());
 app.use('/api/shopTypes', shopTypeRouter);
 app.use('/api/paymentTypes', paymentTypeRouter);
+app.use('/api/productCategories', productCategoryRouter)
 
 app.use((_, res) =>{
     return res.status(404).send({message: 'Resource not found'});
