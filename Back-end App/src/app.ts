0c23a5b12eeedc0dc24c6b8ae4221c542774ad85
maurofv00 <mauroferrarirc@gmail.com>
2025-02-16 18:17:24 -0300

    Brief description and documentation added in app.ts and appConfig.ts

--- Back-end App/src/app.ts
+++ Back-end App/src/app.ts
@@ -1,11 +1,16 @@
-import dotenv from 'dotenv';
-import { getApp } from './appConfig.js';
+// Importación de dependencias
+import dotenv from "dotenv"; // Para cargar variables de entorno desde un archivo .env
+import { getApp } from "./appConfig.js"; // Importar la función getApp para obtener la aplicación configurada
 
+// Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
-const app = getApp()
+// Obtener la aplicación Express configurada
+const app = getApp();
 
-
+// Escuchar el puerto de la API y mostrar un mensaje en consola con la dirección del servidor
 app.listen(process.env.API_PORT, () => {
-    console.log(`Server running on ${process.env.API_SERVER}:${process.env.API_PORT}`)
-})
+  console.log(
+    `Server running on ${process.env.API_SERVER}:${process.env.API_PORT}`
+  );
+});
