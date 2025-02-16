1237f944b4a1858bde65b93a824729df4ddd0fe9
maurofv00 <mauroferrarirc@gmail.com>
2025-02-15 21:11:20 -0300

    Brief description and documentation added in app.ts and appConfig.ts
    brief description and documentation in app.ts and appConfig.ts and swaggerSpec.config.ts

--- Back-end App/src/appConfig.ts
+++ Back-end App/src/appConfig.ts
@@ -1,64 +1,74 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { shopTypeRouter } from './shopType/shopType.routes.js';
-import { paymentTypeRouter } from './paymentType/paymentType.routes.js';
-import { productCategoryRouter } from './productCategory/productCategory.routes.js';
-import { commissionRouter } from './commission/commission.routes.js';
-import { userTypeRouter } from './userType/userType.routes.js';
-import { userRouter } from './user/user.routes.js';
-import { productRouter } from './product/product.routes.js';
-import { withdrawalRouter } from './withdrawal/withdrawal.routes.js';
-import { orderRouter } from './order/order.routes.js';
-import { shopRouter } from './shop/shop.routes.js';
-import { productVariationRouter } from './productVariation/productVariation.routes.js';
-import { reviewRouter } from './review/review.routes.js';
-import { RequestContext } from '@mikro-orm/core';
-import { orm } from './shared/orm.js';
-import { serve, setup } from 'swagger-ui-express';
-import { swaggerSpec } from './swaggerSpec.config.js';
+// Importación de dependencias y módulos
+import express from "express"; // Framework de Node.js para crear el servidor web
+import cors from "cors"; // Middleware para habilitar CORS
+import cookieParser from "cookie-parser"; // Middleware para manejar cookies
+import { shopTypeRouter } from "./shopType/shopType.routes.js"; // Router para shopType
+import { paymentTypeRouter } from "./paymentType/paymentType.routes.js"; // Router para paymentType
+import { productCategoryRouter } from "./productCategory/productCategory.routes.js"; // Router para productCategory
+import { commissionRouter } from "./commission/commission.routes.js"; // Router para commission;
+import { userTypeRouter } from "./userType/userType.routes.js"; // Router para userType
+import { userRouter } from "./user/user.routes.js"; // Router para user
+import { productRouter } from "./product/product.routes.js"; // Router para product
+import { withdrawalRouter } from "./withdrawal/withdrawal.routes.js"; // Router para withdrawal
+import { orderRouter } from "./order/order.routes.js"; // Router para order
+import { shopRouter } from "./shop/shop.routes.js"; // Router para shop
+import { productVariationRouter } from "./productVariation/productVariation.routes.js"; // Router para productVariations
+import { reviewRouter } from "./review/review.routes.js"; // Router para review
+import { RequestContext } from "@mikro-orm/core"; // Contexto de base de datos para MikroORM
+import { orm } from "./shared/orm.js"; // Instancia de MikroORM para manejo de la base de datos
+import { serve, setup } from "swagger-ui-express"; // Middleware para servir la interfaz de Swagger
+import { swaggerSpec } from "./swaggerSpec.config.js"; // Configuración de Swagger para la documentación de la API
 
-export const isProduction = process.env.NODE_ENV === 'production';
+// Verificación del entorno de producción
+export const isProduction = process.env.NODE_ENV === "production";
 
+// Función para configurar y obtener la aplicación Express
 export function getApp() {
-  const app = express();
+  const app = express(); // Crear una instancia de la aplicación Express
 
+  // Orígenes permitidos para CORS
   const allowedOrigins = [
-    'http://localhost:4200',
-    'https://deliverit.vercel.app',
+    "http://localhost:4200",
+    "https://deliverit.vercel.app",
   ];
 
+  // Configuración de CORS
   app.use(
     cors({
-      origin: allowedOrigins,
-      credentials: true,
+      origin: allowedOrigins, // Orígenes permitidos
+      credentials: true, // Habilitar el envío de credenciales (cookies, tokens, etc.)
     })
   );
-  app.use(express.json());
-  app.use(cookieParser());
+  app.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes en formato JSON
+  app.use(cookieParser()); // Middleware para manejar cookies
 
+  // Middleware para crear un contexto de base de datos con MikroORM en cada solicitud
   app.use((req, res, next) => {
     RequestContext.create(orm.em, next);
   });
 
-  app.use('/api/shopTypes', shopTypeRouter);
-  app.use('/api/paymentTypes', paymentTypeRouter);
-  app.use('/api/productCategories', productCategoryRouter);
-  app.use('/api/commissions', commissionRouter);
-  app.use('/api/userTypes', userTypeRouter);
-  app.use('/api/user', userRouter);
-  app.use('/api/products', productRouter);
-  app.use('/api/shops', shopRouter);
-  app.use('/api/order', orderRouter);
-  app.use('/api/withdrawal', withdrawalRouter);
-  app.use('/api/productVariations', productVariationRouter);
-  app.use('/api/reviews', reviewRouter);
+  // Asociación de routers con rutas base
+  app.use("/api/shopTypes", shopTypeRouter); //Rutas para shopTypes
+  app.use("/api/paymentTypes", paymentTypeRouter); //Rutas para paymentTypes
+  app.use("/api/productCategories", productCategoryRouter); //Rutas para productCategories
+  app.use("/api/commissions", commissionRouter); //Rutas para comissions
+  app.use("/api/userTypes", userTypeRouter); //Rutas para userTypes
+  app.use("/api/user", userRouter); //Rutas para user
+  app.use("/api/products", productRouter); //Rutas para products
+  app.use("/api/shops", shopRouter); //Rutas para shops
+  app.use("/api/order", orderRouter); //Rutas para order
+  app.use("/api/withdrawal", withdrawalRouter); //rutas para withdrawal
+  app.use("/api/productVariations", productVariationRouter); //rutas para productVariations
+  app.use("/api/reviews", reviewRouter); //rutas para reviews
 
-  app.use('/api-docs', serve, setup(swaggerSpec));
+  // Middleware para servir la documentación de la API con Swagger UI
+  app.use("/api-docs", serve, setup(swaggerSpec));
 
+  // Middleware para manejar solicitudes a rutas no definidas (error 404)
   app.use((_, res) => {
-    return res.status(404).send({ message: 'Resource not found' });
+    return res.status(404).send({ message: "Resource not found" });
   });
 
+  // Retorna la aplicación Express configurada
   return app;
 }
--- Back-end App/src/swaggerSpec.config.ts
+++ Back-end App/src/swaggerSpec.config.ts
@@ -1,17 +1,18 @@
-import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerJSDoc from "swagger-jsdoc";
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'DeliverIt backend API',
-    version: '1.0.0',
-    description: 'REST API for DeliverIt backend methods',
+    title: "DeliverIt backend API",
+    version: "1.0.0",
+    description: "REST API for DeliverIt backend methods",
   },
 };
 
 const options = {
-  swaggerDefinition,
-  apis: ['./**/*.routes.ts', './**/*.schema.ts'],
+  swaggerDefinition, // Definición base
+  apis: ["./**/*.routes.ts", "./**/*.schema.ts"], // Rutas de los archivos que contienen la documentación
 };
 
+// Exportar la especificación de Swagger generada
 export const swaggerSpec = swaggerJSDoc(options);
